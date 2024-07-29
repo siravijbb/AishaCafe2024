0c695f255662d9be06fe6b0a6ea1e5ae781fc10a
Siravij Praevisavakij <Siravijbb@gmail.com>
2024-07-30 01:16:01 +0700

    edited:color

--- src/components/wishes/Wishbox/SingleWishbox.svelte
+++ src/components/wishes/Wishbox/SingleWishbox.svelte
@@ -16,7 +16,7 @@
 <div id="countLimit" class="mb-16 my-auto mt-1 grid grid-cols-1 gap-80 overscroll-none scroll-auto">
 	<div
 		id="onewish"
-		class="outline-20 mx-auto h-[80vh] w-screen max-w-[349.333px] snap-y snap-mandatory space-y-20 overflow-x-scroll shadow-xl outline outline-offset-2 outline-[#c7722e] md:w-full md:outline-0 md:outline-offset-0"
+		class="outline-20 mx-auto h-[80vh] w-screen max-w-[349.333px] snap-y snap-mandatory space-y-20 overflow-x-scroll shadow-xl outline outline-offset-2 outline-aisha md:w-full md:outline-0 md:outline-offset-0"
 	>
 		{#each Wishdata.wish as wish (wish.count)}
 			{#if wish.picture == 1}
